9c053861bae7162f383df313b5c2a6370f2434a2
axreldable <starikovaleksei1992@mail.ru>
2018-12-14 20:08:39 +0300

    graphics task done

--- main.cpp
+++ main.cpp
@@ -1,119 +1,135 @@
 #include <GL/glut.h>
+#include <cstdio>
+#include "SOIL.h"
+
+// g++ main_texture.cpp -lGL -lGLU -lglut -lSOIL
 
 GLfloat xRotated, yRotated, zRotated;
-GLdouble size=1;
-
-// The coordinates for the vertices of the cube
-double x = 0.6;
-double y = 0.6;
-double z = 0.6;
+GLdouble size = 1;
 float angle = 0.0;
+GLuint tex_2d;
 
 void createCube() {
-    // Create the 3D cube
-    // BACK
     glBegin(GL_POLYGON);
-    glColor3f(0.5, 0.3, 0.2);
-    glVertex3f(x, -y, z);
-    glVertex3f(x, y, z);
-    glVertex3f(-x, y, z);
-    glVertex3f(-x, -y, z);
+    // Передняя грань
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);    // Низ лево
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);    // Низ право
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);    // Верх право
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);    // Верх лево
     glEnd();
-    // FRONT
+
+    // Задняя грань
     glBegin(GL_POLYGON);
-    glColor3f(0.0, 0.5, 0.0);
-    glVertex3f(-x, y, -z);
-    glVertex3f(-x, -y, -z);
-    glVertex3f(x, -y, -z);
-    glVertex3f(x, y, -z);
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);    // Низ право
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);    // Верх право
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);    // Верх лево
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);    // Низ лево
     glEnd();
-    // LEFT
-    glBegin(GL_POLYGON);
-    glColor3f(0.5, 0.5, 0.5);
-    glVertex3f(-x, -y, -z);
-    glVertex3f(-x, -y, z);
-    glVertex3f(-x, y, z);
-    glVertex3f(-x, y, -z);
+
+    // Верхняя грань
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);    // Верх лево
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);    // Низ лево
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);    // Низ право
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);    // Верх право
     glEnd();
-    // RIGHT
+
+    // Нижняя грань
     glBegin(GL_POLYGON);
-    glColor3f(0.0, 0.0, 0.0);
-    glVertex3f(x, -y, -z);
-    glVertex3f(x, -y, z);
-    glVertex3f(x, y, z);
-    glVertex3f(x, y, -z);
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);    // Верх право
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);    // Верх лево
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);    // Низ лево
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);    // Низ право
     glEnd();
-    // TOP
+
+    // Правая грань
     glBegin(GL_POLYGON);
-    glColor3f(0.6, 0.0, 0.0);
-    glVertex3f(x, y, z);
-    glVertex3f(-x, y, z);
-    glVertex3f(-x, y, -z);
-    glVertex3f(x, y, -z);
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);    // Низ право
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);    // Верх право
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);    // Верх лево
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);    // Низ лево
     glEnd();
-    // BOTTOM
+
+    // Левая грань
     glBegin(GL_POLYGON);
-    glColor3f(0.3, 0.0, 0.3);
-    glVertex3f(-x, -y, -z);
-    glVertex3f(-x, -y, z);
-    glVertex3f(x, -y, z);
-    glVertex3f(x, -y, -z);
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);    // Низ лево
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);    // Низ право
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);    // Верх право
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);    // Верх лево
     glEnd();
 }
 
-void display(void)
-{
+void display(void) {
     glMatrixMode(GL_MODELVIEW);
     // clear the drawing buffer.
     glClear(GL_COLOR_BUFFER_BIT);
     // clear the identity matrix.
     glLoadIdentity();
 
+    // включить текстурирование перед кубиком
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, tex_2d);
     glPushMatrix();
-    // traslate the draw by z = -4.0
-    // Note this when you decrease z like -8.0 the drawing will looks far , or smaller.
-    // сдвинули чайник влево вниз
-    glTranslatef(-3.5,-2,-12);  // glTranslate — multiply the current matrix by a translation matrix
-    // Green color used to draw.
-    glColor3f(0.0, 1.0, 0.0);
-    // changing in transformation matrix.
-    // rotation about X axis
-    glRotatef(xRotated,1.0,0.0,0.0); // glRotate — multiply the current matrix by a rotation matrix
-    // rotation about Y axis
-    glRotatef(yRotated,0.0,1.0,0.0);
-    // rotation about Z axis
-    glRotatef(zRotated,0.0,0.0,1.0);
-    // scaling transfomation
-    glScalef(1.0,1.0,1.0); // glScale — multiply the current matrix by a general scaling matrix
-    // built-in (glut library) function , draw you a Teapot.
-    glutSolidTeapot(size);
-    glPopMatrix();
-
-
-    glPushMatrix();
-//    glTranslatef(0,0,-12);
-//    glColor3f(0.0, 1.0, 0.0);
-//    glRotatef(xRotated,1.0,0.0,0.0);
-//    glRotatef(yRotated,0.0,1.0,0.0);
-//    glRotatef(zRotated,0.0,0.0,1.0);
-//    glScalef(1.0,1.0,1.0);
-    glTranslatef(0,0,-12);
+    glTranslatef(2, 2, -12);
     glRotatef(angle, 1.0, 1.0, 1.0);
     glRotatef(angle, 1.0, 0.0, 1.0);
     glRotatef(angle, 0.0, 1.0, 1.0);
     glTranslatef(-0.5, -1.0, 0.0);
     createCube();
     glPopMatrix();
+    glDisable(GL_TEXTURE_2D); // выключаем текстуру
+
+    glPushMatrix();
+    // traslate the draw by z = -4.0
+    // Note this when you decrease z like -8.0 the drawing will looks far , or smaller.
+    // сдвинули чайник влево вниз
+    glTranslatef(-3.5, -2, -12);  // glTranslate — multiply the current matrix by a translation matrix
+    // Green color used to draw.
+    glColor3f(0.439216, 0.858824, 0.576471); // Aquamarine
+    // changing in transformation matrix.
+    // rotation about X axis
+    glRotatef(xRotated, 1.0, 0.0, 0.0); // glRotate — multiply the current matrix by a rotation matrix
+    // rotation about Y axis
+    glRotatef(yRotated, 0.0, 1.0, 0.0);
+    // rotation about Z axis
+    glRotatef(zRotated, 0.0, 0.0, 1.0);
+    // scaling transfomation
+    glScalef(1.0, 1.0, 1.0); // glScale — multiply the current matrix by a general scaling matrix
+    // built-in (glut library) function , draw you a Teapot.
+    glutSolidTeapot(size);
+    glPopMatrix();
 
     glFlush();
     glutSwapBuffers(); // смена экранных буфферов, так как мы используем двойную буфферизацию
 }
 
-void update(int value)
-{
+void update(int value) {
     angle += 1.0f;
-    if (angle > 360)
-    {
+    if (angle > 360) {
         angle -= 360;
     }
 
@@ -121,8 +137,7 @@ void update(int value)
     glutTimerFunc(25, update, 0);
 }
 
-void reshapeFunc(int x, int y)
-{
+void reshapeFunc(int x, int y) {
     if (y == 0 || x == 0) return;  //Nothing is visible then, so return
     //Set a new projection matrix
     glMatrixMode(GL_PROJECTION);
@@ -131,20 +146,48 @@ void reshapeFunc(int x, int y)
     //Near clipping plane distance: 0.5
     //Far clipping plane distance: 20.0
 
-    gluPerspective(40.0,(GLdouble)x/(GLdouble)y,0.5,20.0);
-
-    glViewport(0,0,x,y);  //Use the whole window for rendering
+    gluPerspective(40.0, (GLdouble) x / (GLdouble) y, 0.5, 20.0);
+    glViewport(0, 0, x, y);  //Use the whole window for rendering
 }
 
-void idleFunc(void)
-{
-    yRotated += 0.55;
+void idleFunc(void) {
+    yRotated += 0.01;
     display();
 }
 
+void init_texture() {
+    tex_2d = SOIL_load_OGL_texture(
+            "/home/axreldable/Yandex.Disk/semester_7/graphics/simple_example/basketball.png",
+            SOIL_LOAD_AUTO,
+            SOIL_CREATE_NEW_ID,
+            SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB
+    );
 
-int main (int argc, char **argv)
-{
+    /* check for an error during the load process */
+    if (0 == tex_2d) {
+        printf("SOIL loading error: '%s'\n", SOIL_last_result());
+    } else {
+        glEnable(GL_TEXTURE_2D);
+        glBindTexture(GL_TEXTURE_2D, tex_2d);
+    }
+}
+
+void init_light() {
+    GLfloat light_ambient[] = {0.0, 0.0, 0.0, 1.0};
+    GLfloat light_diffuse[] = {1.0, 1.0, 1.0, 1.0};
+    GLfloat light_specular[] = {1.0, 1.0, 1.0, 1.0};
+    GLfloat light_position[] = {100.0, 100.0, 0.0, 0.0};
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+}
+
+int main(int argc, char **argv) {
     //Initialize GLUT
     glutInit(&argc, argv);
     //double buffering used to avoid flickering problem in animation
@@ -152,15 +195,20 @@ int main (int argc, char **argv)
     // window size
     glutInitWindowSize(800, 600);
     // create the window
-    glutCreateWindow("!!!!!!!!!!!!!!!!!!");
-    glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
-    zRotated = 30.0;
-    xRotated=33;
-    yRotated=40;
+    glutCreateWindow("Teapot-cube dance");
+
+    init_texture();
+    init_light();
+// чтобы не были видны невидимые части кубика
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+
+    zRotated = 30;
+    xRotated = 33;
+    yRotated = 40;
 
     // Set the color of the background
-    glClearColor(0.0,0.0,0.0,0.0);
-
+    glClearColor(0.0, 0.0, 0.0, 0.0);
 
     //Assign  the function used in events
     glutDisplayFunc(display);
--- main_texture.cpp
+++ main_texture.cpp
@@ -1,206 +1,188 @@
+#include <stdlib.h>
+#include <math.h>
+#include <GL/glut.h>
+#include "SOIL.h"
 
-#include "GL/gl.h"
-#include "GL/freeglut.h"
-#include "camera.h"
-#include "textured-shapes.h"
-#include "FreeImage.h"
-#include "stdio.h"
+void init(void)
+{
+    /* texture */
+    GLuint tex_2d = SOIL_load_OGL_texture
+            (
+                    "/home/axreldable/Yandex.Disk/semester_7/graphics/simple_example/basketball.png",
+                    SOIL_LOAD_AUTO,
+                    SOIL_CREATE_NEW_ID,
+                    SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT
+            );
 
-// This program can display any of 8 objects, textured using any of
-// 6 texture images.  The texture images MUST be availalbe in a directory
-// named "textures" in the same directory as the executable.  (Or, you can
-// edit the names in the textureFileNames array.)
-//   The user can change the object by pressing the left and right arrow
-// keys and can change the texture with the up and down arrow keys.  The
-// view can be rotated by dragging the mouse on the window.  The default
-// view can be restored using the HOME key.
-//    This program depends on camera.c and textured-shapes.c, as well as
-// the GL, GLU, GLUT, math, and FreeImage libraries.  Example of compiling
-// it:
-//      gcc texture-demo.c camera.c textured-shapes.c -lGL -lglut -lfreeimage -lm -lGLU
+    glClearColor ( 0.80, 0.85, 0.95, 1.0 );
 
-const int           // code numbers for the 8 objects that can be displayed
-        SPHERE = 0,
-        CYLINDER = 1,
-        CONE = 2,
-        CUBE = 3,
-        TORUS = 4,
-        TEAPOT = 5,
-        SQUARE = 6,
-        CIRCLE = 7,
-        RING = 8;
-
-int currentTexture = 0;  // code number (0 to 7) for the object that is currently displayed
-
-int currentObject = 0;   // code number (0 to 5) for the texture used on the object
-
-char* textureFileNames[6] = {   // file names for the files from which texture images are loaded
-        "textures/Earth-1024x512.jpg",
-        "textures/NightEarth-512x256.jpg",
-        "textures/brick001.jpg",
-        "textures/marble.jpg",
-        "textures/metal003.gif",
-        "textures/mandelbrot.jpeg"
-};
-void* imgData[6];  // Pointer to raw RGB data for textures.
-int imgWidth[6];   // Widths of texture images.
-int imgHeight[6];  // Heights of texture imgages.
-
-void loadTextures() {
-    // loads the 6 textures using the FreeImage library, and
-    // stores the required info in the imgData, imgWidth, imgHeight arrays.
-    int i;
-    printf("LOADING TEXTURES...\n");
-    for (i = 0; i < 6; i++) {
-        imgData[i] = 0;
-        FREE_IMAGE_FORMAT format = FreeImage_GetFIFFromFilename(textureFileNames[i]);
-        if (format == FIF_UNKNOWN) {
-            printf("Unknown file type for texture image file %s\n", textureFileNames[i]);
-            continue;
-        }
-        FIBITMAP* bitmap = FreeImage_Load(format, textureFileNames[i], 0);
-        if (!bitmap) {
-            printf("Failed to load image %s\n", textureFileNames[i]);
-            continue;
-        }
-        FIBITMAP* bitmap2 = FreeImage_ConvertTo24Bits(bitmap);
-        FreeImage_Unload(bitmap);
-        imgData[i] = FreeImage_GetBits(bitmap2);
-        imgWidth[i] = FreeImage_GetWidth(bitmap2);
-        imgHeight[i] = FreeImage_GetHeight(bitmap2);
-        if (imgData[i]) {
-            printf("Texture image loaded from file %s, size %dx%d\n",
-                   textureFileNames[i], imgWidth[i], imgHeight[i]);
-        }
-        else {
-            printf("Failed to get texture data from %s\n", textureFileNames[i]);
-        }
+    /* check for an error during the load process */
+    if( 0 == tex_2d )
+    {
+        //printf( "SOIL loading error: '%s'\n", SOIL_last_result() );
+    } else {
+        glEnable( GL_TEXTURE_2D );
+        glBindTexture( GL_TEXTURE_2D, tex_2d );
     }
-    printf("DONE\n\n");
-}
 
+    /* light */
+    GLfloat light_ambient[] =
+            {0.8, 0.85, 0.95, 1.0};
+    GLfloat light_diffuse[] =
+            {1.0, 0.95, 0.8, 1.0};
+    GLfloat light_specular[] =
+            {1.0, 0.95, 0.8, 1.0};
+    GLfloat light_position[] =
+            {1.0, 0.5, 1.0, 0.0};
 
-void initGL() {
-    // Initialization function, called by main() after creating the window.
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+    glEnable(GL_LIGHT0);
+    glDepthFunc(GL_LEQUAL);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
-    glEnable(GL_NORMALIZE);
-    glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, 1);
-    float diffuse[4] = {1,1,1,1};
-    float spec[4] = {0.3f, 0.3f, 0.3f, 1};
-    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, diffuse);
-    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, spec);
-    glMateriali(GL_FRONT_AND_BACK, GL_SHININESS, 100);
-    glLightModeli(GL_LIGHT_MODEL_COLOR_CONTROL, GL_SEPARATE_SPECULAR_COLOR);
-    // I am cheating by using separate specular color, which requires OpenGL 1.2, but
-    // it gives nicer specular highlights on textured surfaces.
-    glClearColor(0.0, 0.0, 0.0, 1.0);
-    glColor3f(1,1,1);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);  // (Actually, this one is the default)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glEnable(GL_TEXTURE_2D);
-    cameraSetScale(1);
-    loadTextures();
+
+    glHint ( GL_POLYGON_SMOOTH_HINT,         GL_NICEST );
+    glHint ( GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST );
 }
 
+void drawBox (GLfloat x1, GLfloat x2, GLfloat y1, GLfloat y2, GLfloat z1, GLfloat z2)
+{
+    glBegin(GL_POLYGON);
 
-void renderFunction() {
-    // draw the current object using the current texture.
+    glNormal3f(0.0, 0.0, 1.0);
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(x1, y1, z2);
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(x2, y1, z2);
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(x2, y2, z2);
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(x1, y2, z2);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glNormal3f(0.0, 0.0, -1.0);
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(x2, y1, z1);
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(x1, y1, z1);
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(x1, y2, z1);
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(x2, y2, z1);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glNormal3f(-1.0, 0.0, 0.0);
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(x1, y1, z1);
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(x1, y1, z2);
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(x1, y2, z2);
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(x1, y2, z1);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glNormal3f(1.0, 0.0, 0.0);
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(x2, y1, z2);
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(x2, y1, z1);
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(x2, y2, z1);
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(x2, y2, z2);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glNormal3f(0.0, 1.0, 0.0);
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(x1, y2, z2);
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(x2, y2, z2);
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(x2, y2, z1);
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(x1, y2, z1);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glNormal3f(0.0, -1.0, 0.0);
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(x2, y1, z2);
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(x1, y1, z2);
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(x1, y1, z1);
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(x2, y1, z1);
+    glEnd();
+}
+
+GLfloat angle = 0.0;
+GLfloat height = 0.0;
+
+void display(void)
+{
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    cameraApply();
+    glPushMatrix();
+    glRotatef(angle, 0.0, 0.0, 1.0);
+    glRotatef(angle, 1.0, 0.0, 0.0);
+    glTranslatef(0.0, 0.0, angle / 100.0f);
+    drawBox(1.0, 2.0, 0.0, 1.0, 0.0, 1.0);
+    glPopMatrix();
 
-    if (imgData[currentTexture]) {
-        int format = (FI_RGBA_RED == 0) ? GL_RGB : GL_BGR;
-        glEnable(GL_TEXTURE_2D);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, imgWidth[currentTexture], imgHeight[currentTexture],
-                     0, format, GL_UNSIGNED_BYTE, imgData[currentTexture]);
-    }
-    else {
-        printf("Missing texture number %d\n", currentTexture);
-        glDisable(GL_TEXTURE_2D);
-    }
+    glPushMatrix();
+    glTranslatef(0.0, height, 0.0);
+    drawBox(0.0, 1.0, 0.0, 1.0, 3.0, 4.0);
+    glPopMatrix();
 
-    if (currentObject <= CONE)
-        glRotated(90,-1,0,0);  // rotate axis of object from z-axis to y-axis
-    if (currentObject == CONE || currentObject == CYLINDER)
-        glTranslated(0,0,-0.5);  // moves center of object to the origin
-
-    if (currentObject == SPHERE)
-        uvSphere1();
-    else if (currentObject == CYLINDER)
-        uvCylinder1();
-    else if (currentObject == CONE)
-        uvCone1();
-    else if (currentObject == CUBE)
-        cube1();
-    else if (currentObject == TORUS)
-        uvTorus1();
-    else if (currentObject == TEAPOT) {
-        glFrontFace(GL_CW);  // Teapot has non-standard front faces
-        // that don't work right with two-sided lighting.
-        // This reverses the usual test for front face.
-        glutSolidTeapot(0.5);
-        glFrontFace(GL_CCW);
-    }
-    else if (currentObject == SQUARE)
-        square1();
-    else if (currentObject == CIRCLE)
-        circle1();
-    else if (currentObject == RING)
-        ring1();
-
-    glutSwapBuffers();
+    glFlush();
 }
 
-
-void specialKeyFunction(int key, int x, int y) {
-    // responds when the user presses a special key.
-
-    if ( key == GLUT_KEY_UP ) {
-        currentTexture++;
-        if (currentTexture > 5)
-            currentTexture = 0;
-    }
-    else if ( key == GLUT_KEY_DOWN ) {
-        currentTexture--;
-        if (currentTexture < 0 )
-            currentTexture = 5;
-    }
-    else if ( key == GLUT_KEY_LEFT ) {
-        currentObject--;
-        if (currentObject < 0)
-            currentObject = 8;
-    }
-    else if ( key == GLUT_KEY_RIGHT ) {
-        currentObject++;
-        if (currentObject > 8)
-            currentObject = 0;
-    }
-    else if ( key == GLUT_KEY_HOME ) {
-        cameraLookAt(0,0,30, 0,0,0, 0,1,0);
-    }
+void idle(void) {
+    angle = sin(glutGet ( GLUT_ELAPSED_TIME ) * 0.00015f) * 360.0f;
+    height = 0.1f * sin(glutGet(GLUT_ELAPSED_TIME) * 0.003);
     glutPostRedisplay();
 }
 
-int main(int argc, char** argv) {
+void reshape(int w, int h)
+{
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    gluPerspective ( 60.0, (GLfloat)w/(GLfloat)h, 1.0, 60.0 );
+    gluLookAt(2.0, -2.0, 8.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+void key(unsigned char key, int x, int y)
+{
+    if ( key == 27 || key == 'q' || key == 'Q' )    //    quit requested
+        exit ( 0 );
+}
+
+int main(int argc, char **argv)
+{
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
-    glutInitWindowSize(600,600);
-    glutInitWindowPosition(100,100);
-    glutCreateWindow("USE ARROW KEYS TO CHANGE TEXTURES AND OBJECTS");
-    initGL();
-    glutDisplayFunc(renderFunction);
-    glutSpecialFunc(specialKeyFunction);
-    glutMouseFunc(trackballMouseFunction);    // install trackball, step 1
-    glutMotionFunc(trackballMotionFunction);  // install trackball, step 2 
-    printf("The left and right arrow keys will change the object.\n");
-    printf("The up and down arrow keys will change the texture.\n");
-    printf("The HOME key restores the original point of view.\n");
-    printf("The mouse can be used to rotate the view.\n");
+    glutInitWindowPosition(500, 500);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("Computer Graphics Task");
+    init();
+    glutReshapeFunc(reshape);
+    glutDisplayFunc(display);
+    glutIdleFunc(idle);
+    glutKeyboardFunc(key);
     glutMainLoop();
     return 0;
 }
